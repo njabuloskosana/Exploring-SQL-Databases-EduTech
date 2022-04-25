09be868616dc979d191ae125d6246585eddb9ebb
Njabulo <njabulo@xcontent.com>
2022-04-25 19:01:50 +0200

    Sql assignment setup

---
+++ samples/assignment-template/java-code/sql-database/src/App.java
@@ -0,0 +1,52 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        SqlDatabaseQueries atm = new SqlDatabaseQueries();
+        boolean response=false;
+        
+            Scanner scan = new Scanner(System.in);
+            scan.useLocale(Locale.US);
+            int x;
+            atm.showMenu();
+            x = scan.nextInt();
+
+            switch (x) {
+            case 1:
+                response=atm.createCard();
+                break;
+
+            case 2:
+                response=atm.getBalance();
+                scan.close();
+                break;
+
+            case 3:
+                response=atm.deposit();
+                scan.close();
+                break;
+
+            case 4:
+                response=atm.withDraw();
+                scan.close();
+                break;
+
+            case 5:
+                response=atm.printStatement();
+                scan.close();
+                break;
+            case -1:
+                scan.close();
+                break;
+            
+            default:
+                System.out.println("Wrong choice");
+                break;
+            }
+
+        
+    }
+}
+
---
+++ samples/assignment-template/java-code/sql-database/src/SqlDatabase.java
@@ -0,0 +1,88 @@
+public class SqlDatabase {
+
+    /**
+     * Note : You are required to add your own arguments in all the functions so that 
+     * You have all the data required to store information into the database
+     */
+
+    /**
+     * This function will print out a numbered options the user can select 
+     * to perform a transaction with the ATM
+     */
+    public void showMenu()
+    {
+
+    }
+
+    /**
+     * This function will create a new card for a client
+     * This function will also add a new client into the database
+     * If the user has already been added print out user already has a profile to the terminal
+     * Add missing parameters
+     * @return
+     */
+    public boolean createCard()
+    {
+        return false;
+    }
+
+
+    /**
+     * This function will add a new client into the database
+     * This function will be called by create card
+     * Add missing parameters
+     * @return
+     */
+    public boolean addClient()
+    {
+        return false;
+    }
+    
+
+    /**
+     * This returns the balance of the client
+     * The function will also update the transaction records and reports after a balance inquiry transaction is performed
+     * Add missing parameters
+     * @return
+     */
+    public boolean getBalance() {
+        return false;
+    }
+
+
+    /**
+     * This function deposits money into the users savings account
+     * The function will also update the transaction records and reports after a savings transaction is performed
+     * Add missing parameters
+     * @return
+     */
+    public boolean deposit()
+    {
+        return false;
+    }
+
+    /**
+     * This function withdraws money from the users savings account
+     * The function will also update the transaction records and reports after a withdraw transaction is performed
+     * Add missing parameters
+     * @return
+     */
+    public boolean withDraw(){
+        return false;
+    }
+
+   
+    /**
+     * This fuction will print out all the reported transactions of a client
+     * And the balance or ammount associated with that specific transaction
+     * Add missing parameters
+     * @return
+     */
+    public boolean printStatement(){
+
+        return false;
+    }
+
+    
+}
+
